dad34e6efe6ff793b1ccdf91c9b713e4e4e3326b
Ryan <ryanpvoong@gmail.com>
2023-01-07 22:47:29 -0500

    updating env

--- serviceAccount.js
+++ serviceAccount.js
@@ -2,7 +2,7 @@ module.exports = {
   type: process.env.FIREBASE_SERVICE_ACCOUNT_type,
   project_id: process.env.FIREBASE_SERVICE_ACCOUNT_project_id,
   private_key_id: process.env.FIREBASE_SERVICE_ACCOUNT_private_key_id,
-  private_key: process.env.FIREBASE_SERVICE_ACCOUNT_private_key,
+  private_key: process.env.FIREBASE_SERVICE_ACCOUNT_private_key ? process.env.FIREBASE_SERVICE_ACCOUNT_private_key.replace(/\\n/g, '\n') : undefined,
   client_email: process.env.FIREBASE_SERVICE_ACCOUNT_client_email,
   client_id: process.env.FIREBASE_SERVICE_ACCOUNT_client_id,
   auth_uri: process.env.FIREBASE_SERVICE_ACCOUNT_auth_uri,
